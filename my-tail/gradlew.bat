d6a4fc68b12f2bed6d119b0474ac0d118ded9aa4
胡毅宇 <huyiyudesigner@163.com>
2025-03-01 19:07:35 +0800

    补充产品文档

--- my-tail/gradlew.bat
+++ my-tail/gradlew.bat
@@ -22,7 +22,7 @@
 @rem  Gradle startup script for Windows
 @rem
 @rem ##########################################################################
-
+@echo off
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
